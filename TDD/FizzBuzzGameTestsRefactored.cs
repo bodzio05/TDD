ee84a356547fb381e940faaf254e7074d930cc63
Marcin Cierlik <marcin.cierlik@gmail.com>
2019-12-17 12:12:27 +0100

    Fizz buzz tests - refactored; first working version

---
+++ TDD/FizzBuzzGameTestsRefactored.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDD
+{
+    public class FizzBuzzGameTestsRefactored
+    {
+        [Theory,
+            InlineData(1,"1"),
+            InlineData(2,"2"),
+            InlineData(3,"Fizz"),
+            InlineData(5,"Buzz"),
+            InlineData(15,"Fizz Buzz")]
+        public void Should_return_values_according_to_Fizz_Buzz_game_rulez(int input, string expected)
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(input);
+
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
