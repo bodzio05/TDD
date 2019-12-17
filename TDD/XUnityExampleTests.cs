c58d346f8dfaa7aa832b4f5332f0f8eff06a05eb
Marcin Cierlik <marcin.cierlik@gmail.com>
2019-12-17 10:40:19 +0100

    First test example

---
+++ TDD/XUnityExampleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDD
+{
+    public class XUnityExampleTests: IDisposable
+    {
+        public XUnityExampleTests()
+        {
+
+        }
+
+        [Fact]
+        public void TestName()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.True(true);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
